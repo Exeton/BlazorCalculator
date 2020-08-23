48dae37676e86581fab2bc9da783b0907d925d5e
Exeton <benjamin.diederich@gmail.com>
2020-08-23 14:54:02 -0400

    Started work on calculator

---
+++ LatestBlazorCalculator/LatestBlazorCalculator/Calculator/Calculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatestBlazorCalculator.Calculator
+{
+	public class Calculator
+	{
+
+		private double? Num1;
+		private double? Num2;
+		private Operator? operator_;
+
+		public string Calculation
+		{
+			get;
+			private set;
+		}
+
+		public string Value
+		{
+			get;
+			private set;
+		}
+
+
+		public void OnOperatorInput(Operator newOperator)
+		{
+
+			if (newOperator.SingleNumberOperator())
+			{
+
+			}
+
+
+			/*
+			if (operator_ == null)
+				operator_ = newOperator;
+			else
+				Num1
+			*/
+		}
+
+		private int Calculate(int a, int b, Operator op)
+		{
+			return 0;
+		}
+
+
+		public void OnNumberInput(int number)
+		{
+			if (operator_ == null)
+				Num1 = AppendNumber(Num1.Value, number);
+			else
+				Num2 = AppendNumber(Num2.Value, number);
+		}
+
+		private double AppendNumber(double num, int newDigit)
+		{
+			return num * 10 + newDigit;
+		}
+
+		private string GetText()
+		{
+			if (Num1 == null)
+				return "0";
+
+			if (operator_ == null)
+				return Num1.ToString();
+
+			if (Num2 == null)
+				return Num1.ToString() + " " + operator_.ToString();
+
+			return Num1.ToString() + " " + operator_.ToString() + " " + Num2.ToString();
+
+		}
+
+	}
+
+
+
+}
---
+++ LatestBlazorCalculator/LatestBlazorCalculator/Calculator/OperatorUtil.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatestBlazorCalculator.Calculator
+{
+	public enum Operator
+	{
+		ADD,
+		SUBTRACT,
+		MULTIPLY,
+		DIVIDE,
+		SIN,
+		COS,
+		TAN,
+		SQRT,
+		SQUARE,
+		LOG,
+		LN
+	}
+
+	public static class OperatorUtil
+	{
+		public static bool SingleNumberOperator(this Operator @operator)
+		{
+			switch (@operator)
+			{
+				case Operator.ADD:
+					return false;
+				case Operator.COS:
+					return true;
+				case Operator.DIVIDE:
+					return false;
+				case Operator.LN:
+					return true;
+				case Operator.LOG:
+					return true;
+				case Operator.MULTIPLY:
+					return false;
+				case Operator.SIN:
+					return true;
+				case Operator.SQRT:
+					return true;
+				case Operator.SQUARE:
+					return true;
+				case Operator.SUBTRACT:
+					return false;
+				case Operator.TAN:
+					return true;
+			}
+
+			throw new Exception("Not implemented operator");
+		}
+	}
+}
